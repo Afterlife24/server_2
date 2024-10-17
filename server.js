eac3086429b257c8a92a775438fac708ee144244
Afterlife24 <afterlife668@gmail.com>
2024-10-17 23:47:52 +0530

    tokenremove2

--- server.js
+++ server.js
@@ -59,10 +59,10 @@ app.post("/markAsDelivered", async (req, res) => {
     }
 });
 
-const PORT = 5000;
-app.listen(PORT, function() { 
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, function() { 
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 
-// module.exports = app;
+module.exports = app;
